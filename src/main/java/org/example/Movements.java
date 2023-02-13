789c1c68d985618e855938bc5c6d73600922bcbe
Raymond <raymond.yiu@gmail.com>
2023-02-13 09:21:17 +0000

    define a map of label for Movements enum

--- src/main/java/org/example/Movements.java
+++ src/main/java/org/example/Movements.java
@@ -1,6 +1,26 @@
 package org.example;
 
-public enum Movements {
-    MoveForward, TurnLeft, TurnRight
+import javax.lang.model.element.Element;
+import java.util.HashMap;
+import java.util.Map;
 
+public enum Movements {
+    MoveForward("M"), TurnLeft("L"), TurnRight("R");
+    private static final Map<String, Movements> BY_LABEL = new HashMap<>();
+
+    static {
+        for (Movements move : values()) {
+            BY_LABEL.put(move.label, move);
+        }
+    }
+    public final String label;
+
+    public static Movements valueOfLabel(String label) {
+        return BY_LABEL.get(label);
+    }
+
+
+    Movements(String label) {
+        this.label = label;
+    }
 }
